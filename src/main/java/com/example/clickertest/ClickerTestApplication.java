b4490279eae1fa78510a0a83d32b6ab16b83d7ed
Rornoa <joniker.e@mail.ru>
2022-02-18 12:22:22 +0300

    Начальный коммит. Добавлены к отслеживанию версий: src и pom.xml

---
+++ src/main/java/com/example/clickertest/ClickerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.clickertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clickertest/ClickerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clickertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
